865f2d2d6e28d7d7bba0c3e5f86f4943593a1e11
pradeep <psahai26@yahoo.in>
2019-08-24 20:27:35 +0530

    added

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+      "The Google Pixel mobile features a 5.0 (12.7 cm) display with a screen resolution of Full HD (1080 x 1920 pixels) and runs on Android v7.1.1 (Nougat) operating system. The device is powered by Quad core (2.15 GHz, Dual core, Kryo + 1.6 GHz, Dual core, Kryo) processor paired with 4 GB of RAM. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "Samsung Galaxy S7 is powered by a 1.6GHz octa-core processor. It comes with 4GB of RAM. The Samsung Galaxy S7 runs Android 6.0 and is powered by a 3,000mAh non-removable battery. ... Samsung Galaxy S7 based on Android 6.0 and packs 32GB of inbuilt storage that can be expanded via microSD card (up to 200GB).",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "The phone comes with a 5.20-inch touchscreen display with a resolution of 1440x2560 pixels at a pixel density of 564 pixels per inch (ppi). HTC 10 is powered by a 1.6GHz quad-core Qualcomm Snapdragon 820 processor. It comes with 4GB of RAM. The HTC 10 runs Android 6 and is powered by a 3,000mAh non-removable battery.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "The phone comes with a 5.20-inch touchscreen display with a resolution of 1440x2560 pixels at a pixel density of 564 pixels per inch (ppi). HTC 10 is powered by a 1.6GHz quad-core Qualcomm Snapdragon 820 processor. It comes with 4GB of RAM. The HTC 10 runs Android 6 and is powered by a 3,000mAh non-removable battery.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "HTC Desire 626 Dual SIM is powered by a 1.7GHz octa-core MediaTek MT6752 processor. It comes with 2GB of RAM. The HTC Desire 626 Dual SIM runs Android 4.4.4 and is powered by a 2,000mAh non-removable battery. As far as the cameras are concerned, the HTC Desire 626 Dual SIM on the rear packs 13-megapixel camera",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "Vintage products are those that have not been manufactured for more than 5 and less than 7 years ago. Apple has discontinued hardware service for vintage products with the following exceptions:Mac products purchased in the country of Turkey. Owners of vintage Mac products may obtain service and parts from Apple service providers within the country of Turkey.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "The Apple iPhone 7 mobile features a 4.7  (11.94 cm) display with a screen resolution of 750 x 1334 pixels and runs on iOS v10 operating system. The device is powered by Quad core (2.34 GHz, Dual core, Hurricane + 1.1 GHz, Dual core, Zephyr) processor paired with 2 GB of RAM.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 2,
+    company: "apple",
+    info:
+      "I've always been careless with my phones. I've broken phones with the sheer force of my butt by sitting on them, and I've lost them by having them fall out of my pants pockets (I realize now that I should just stop putting phones in my back pocket). I don't know what it says about me that the one essential item in my life that I can't live without is also the thing I treat the worst. Why do we hurt the ones we love the most?",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "",
+  inCart: false,
+  count: 0,
+  total: 0
+};
